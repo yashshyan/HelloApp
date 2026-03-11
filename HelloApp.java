a19c4a5c0ad7bed9c9612a214660d1ef8812960e
yashshyan <yashshyanys1162@gmail.com>
2026-03-11 15:32:55 +0530

    UC2: print Hello World message

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,11 @@
-public class HelloApp {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+public class HelloApp {
+    public static void main(String[] args) {
+
+        if (args.length > 0) {
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello World");
+        }
+
     }
 }
